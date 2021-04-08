f923f42ea265303d1c31c3f39d9b391d2be29ae4
Arthur Targaryen <arthur.targaryen@protonmail.com>
2021-04-08 22:54:51 +0200

    feat(Vec3): implement AddAssign, MulAssign and DivAssign traits

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,5 +1,5 @@
 use std::cmp::PartialEq;
-use std::ops::{Add, Neg};
+use std::ops::{Add, AddAssign, DivAssign, MulAssign, Neg};
 
 #[derive(Debug)]
 pub struct Vec3 {
@@ -64,6 +64,30 @@ impl Add for Vec3 {
     }
 }
 
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        self.x /= rhs;
+        self.y /= rhs;
+        self.z /= rhs;
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -89,5 +113,26 @@ mod test {
             Vec3::new(1.0, 2.0, 3.0) + Vec3::new(1.0, 2.0, 3.0),
             Vec3::new(2.0, 4.0, 6.0)
         );
+    }
+
+    #[test]
+    fn add_assign() {
+        let mut v = Vec3::new(1.0, 2.0, 3.0);
+        v += Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(v, Vec3::new(2.0, 4.0, 6.0))
+    }
+
+    #[test]
+    fn mul_assign() {
+        let mut v = Vec3::new(1.0, 2.0, 3.0);
+        v *= 2.0;
+        assert_eq!(v, Vec3::new(2.0, 4.0, 6.0))
+    }
+
+    #[test]
+    fn div_assign() {
+        let mut v = Vec3::new(2.0, 4.0, 6.0);
+        v /= 2.0;
+        assert_eq!(v, Vec3::new(1.0, 2.0, 3.0))
     }
 }
