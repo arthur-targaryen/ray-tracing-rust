d87a032a31b3777bf6c2b439e973862f02b15011
Arthur Targaryen <arthur.targaryen@protonmail.com>
2021-04-09 01:17:19 +0200

    feat: use Ray struct and its methods

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
 use std::io;
 
-use ray_tracing::color::Color;
+use ray_tracing::ray::Ray;
+use ray_tracing::vec3::{Point3, Vec3};
 
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
 
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::null();
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    // Render
     println!("P3\n{} {}\n255", image_width, image_height);
 
     for j in (0..image_height).rev() {
         eprint!("\rScanlines remaining: {} ", j);
         for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
-            let b = 0.25;
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
 
-            let pxl_color = Color::new(r, g, b);
-            pxl_color
+            r.color()
                 .write(io::stdout())
                 .expect("There was an error trying to write the image to the standard output");
         }
