b5ee420896b10d5ba3bdb932e1a47ea64d03527d
Arthur Targaryen <arthur.targaryen@protonmail.com>
2021-04-19 19:43:56 +0200

    feat(Ray): add naive sphere intersection

--- src/ray.rs
+++ src/ray.rs
@@ -16,15 +16,32 @@ impl Ray {
         self.origin
     }
 
+    #[allow(dead_code)]
     pub fn direction(&self) -> Vec3 {
         self.direction
     }
 
+    #[allow(dead_code)]
     pub fn at(&self, t: f64) -> Point3 {
         self.origin + t * self.direction
     }
 
+    fn hit_sphere(&self, center: &Point3, radius: f64) -> bool {
+        let oc = self.origin() - *center;
+
+        let a = self.direction.dot(&self.direction);
+        let b = 2.0 * oc.dot(&self.direction);
+        let c = oc.dot(&oc) - radius * radius;
+        let discriminant = b * b - 4.0 * a * c;
+
+        discriminant > 0.0
+    }
+
     pub fn color(&self) -> Color {
+        if self.hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5) {
+            return Color::new(1.0, 0.0, 0.0);
+        }
+
         let unit_direction = self.direction.unit_vector();
         let t = 0.5 * (unit_direction.y() + 1.0);
         (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(1.0, 0.7, 0.5)
