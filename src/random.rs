46cad099747897fba94129628c5ed496c353ba17
Arthur Targaryen <arthur.targaryen@protonmail.com>
2021-04-25 23:34:56 +0200

    feat(random): add useful wrappers

---
+++ src/random.rs
@@ -0,0 +1,16 @@
+use std::ops::RangeInclusive;
+
+use rand::{thread_rng, Rng};
+
+// TODO: Cache the ThreadRng for each thread.
+
+/// Return a randol number in [0.0; 1.0).
+pub fn random() -> f64 {
+    thread_rng().gen_range(0.0..1.0)
+}
+
+/// Return a random number in the given range.
+#[allow(dead_code)]
+pub fn random_range(range: RangeInclusive<f64>) -> f64 {
+    thread_rng().gen_range(range)
+}
