ffc3efa22049f7eeb9f763731ba024518ed986e8
Arthur Targaryen <arthur.targaryen@protonmail.com>
2021-04-26 00:23:45 +0200

    feat(random): change random_range() parameter type

--- src/random.rs
+++ src/random.rs
@@ -1,4 +1,4 @@
-use std::ops::RangeInclusive;
+use std::ops::Range;
 
 use rand::{thread_rng, Rng};
 
@@ -10,7 +10,6 @@ pub fn random() -> f64 {
 }
 
 /// Return a random number in the given range.
-#[allow(dead_code)]
-pub fn random_range(range: RangeInclusive<f64>) -> f64 {
+pub fn random_range(range: Range<f64>) -> f64 {
     thread_rng().gen_range(range)
 }
