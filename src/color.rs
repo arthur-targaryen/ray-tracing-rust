1f2725190f28dc0a37607e422d60f035fc36da2b
Arthur Targaryen <arthur.targaryen@protonmail.com>
2021-04-25 23:36:05 +0200

    feat: use Camera struct and compute color with multiple samples

--- src/color.rs
+++ src/color.rs
@@ -7,13 +7,27 @@ pub type Color = vec3::Vec3;
 
 impl Color {
     /// Write a single pixel's color out to a writer.
-    pub fn write(&self, mut stream: impl Write) -> std::io::Result<()> {
+    ///
+    /// The color is the sum of multiple samples, thus this function will
+    /// scale the color and clamp it to an RGB value.
+    pub fn write(&self, mut stream: impl Write, samples_per_pixel: u32) -> std::io::Result<()> {
+        let mut r = self.x();
+        let mut g = self.y();
+        let mut b = self.z();
+
+        // Divide the color by the number of samples.
+        let scale = 1.0 / samples_per_pixel as f64;
+        r *= scale;
+        g *= scale;
+        b *= scale;
+
+        // Write the translated [0; 255] value of each color component.
         writeln!(
             stream,
             "{} {} {}",
-            (255.999 * self.x()) as i32,
-            (255.999 * self.y()) as i32,
-            (255.999 * self.z()) as i32,
+            (256.0 * r.clamp(0.0, 0.999)) as i32,
+            (256.0 * g.clamp(0.0, 0.999)) as i32,
+            (256.0 * b.clamp(0.0, 0.999)) as i32,
         )
     }
 }
